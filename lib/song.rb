da59a6f9e33623f650cc877b742e67a45270c402
Nancy Zaky <nancyzki@yahoo.com>
2021-09-07 14:52:46 -0700

    commit tree 158200fe1634b2d94cf26f29f84c5b35e6a5892b

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,9 @@ class Song
 
     DB[:conn].execute(sql)
   end
-
+  def self.new_from_db(row)
+    Song.new(id: row[0], name: row[1], album: row[2])
+  end
   def save
     sql = <<-SQL
       INSERT INTO songs (name, album)
@@ -47,6 +49,30 @@ class Song
   def self.create(name:, album:)
     song = Song.new(name: name, album: album)
     song.save
+  end
+
+  def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT *
+    FROM songs
+    WHERE name = ?
+    LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+    self.new_from_db(row)
+  end.first
   end
 
 end
